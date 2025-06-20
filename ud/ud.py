6f9fb573bf2389db9e4cfc47735509698857c903
cybersteve547 <minecraftstevso@gmail.com>
2025-06-20 09:38:19 +0000

    helppp

--- ud/ud.py
+++ ud/ud.py
@@ -7,11 +7,11 @@ print("who should i get data on?")
 name = input("")
 limits = [10, 5, 5]
 max_depth = 3
-tree = []
 visited = []
 cu.clear()
 
 def maketree(username, lvl):
+    tree = []
     if lvl > 0:
         indent = ("│" * (lvl - 1)) + "├"
     else:
@@ -19,21 +19,19 @@ def maketree(username, lvl):
     try:
         user = session.connect_user(username)
         if username in visited:
-            tree.append(indent + username + " (already seen)")
             print(indent + username + " (already seen)")
         else:
             visited.append(username)
-            tree.append(indent + username)
             print(indent + username)
             if lvl < max_depth and lvl < len(limits):
                 for follower in user.following(limit=limits[lvl]):
+                    tree.append(follower.username)
                     maketree(follower.username, lvl + 1)
     except Exception as e:
-        tree.append(indent + username + " (broken user)")
         print(indent + username + " (broken user) error: " + str(e))
+    with open(username + ".txt", "w") as file:
+        file.write(username + " is following\n")
+        for i in tree:
+            file.write(i + "\n"
 
 maketree(name, 0)
-with open(name + ".txt", "w") as file:
-    file.write("tree of who " + name + " is following on scratch\n")
-    for i in tree:
-        file.write(i + "\n")
